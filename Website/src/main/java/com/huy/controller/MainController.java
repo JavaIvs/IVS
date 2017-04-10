766aeca9b2e2816aee79cc5bcfa58b9133d32567
HuyNguyen <freeline.g.one@gmail.com>
2017-04-11 01:01:40 +0700

    Đăng nhập và phân quyền hệ thống
    Thực hiện được phần quyền user và member

--- Website/src/main/java/com/huy/controller/MainController.java
+++ Website/src/main/java/com/huy/controller/MainController.java
@@ -1,23 +1,35 @@
 package com.huy.controller;
 
-import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MainController {
 	
-
-    @GetMapping("/")
-    public String index() {
-        return "index";
-    }
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/admin") 
+	public String admin() {
+		return "master";
+	}
+	
+	@GetMapping("/user") 
+	public String user() {
+		return "user";
+	}
+	
+	@GetMapping("/403")
+	public String accessDenied() {
+		return "403";
+	}
+	
+	@GetMapping("/login") 
+	public String getLogin() {
+		return "login";
+	}
+	
 }
